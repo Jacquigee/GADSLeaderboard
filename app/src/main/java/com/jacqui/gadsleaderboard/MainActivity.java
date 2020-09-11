c6468b2ec254b08f19bcbced0b9c15f71453ead0
jacqui <gitaujaquiline@gmail.com>
2020-09-11 09:31:53 +0300

    Making adjustments in the ViewPager

--- app/src/main/java/com/jacqui/gadsleaderboard/MainActivity.java
+++ app/src/main/java/com/jacqui/gadsleaderboard/MainActivity.java
@@ -8,29 +8,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class MainActivity extends AppCompatActivity {
-
-    //ArrayList<LearningHours>  learningHours = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -71,28 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
 
-//    private void getLearnerResponse() {
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("https://gadsapi.herokuapp.com/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//
-//        RequestInterface requestInterface = retrofit.create(RequestInterface.class);
-//        Call<List<LearningHours>> call = requestInterface.getLearningJson();
-//
-//        call.enqueue(new Callback<List<LearningHours>>() {
-//            @Override
-//            public void onResponse(Call<List<LearningHours>> call, Response<List<LearningHours>> response) {
-//                learningHours=new ArrayList<>(response.body());
-//                Toast.makeText(MainActivity.this,"Success", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<LearningHours>> call, Throwable t) {
-//                Toast.makeText(MainActivity.this,"Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
+    }
 }
